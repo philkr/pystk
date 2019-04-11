e1af7ea62cd56a42c9376d850d35eae792616a19
philkr <philkr@users.noreply.github.com>
2019-04-11 00:46:14 -0500

    setup.py implemented

--- pystk/__init__.py
+++ pystk/__init__.py
@@ -1,2 +1,5 @@
+from os import environ, path
+if 'SUPERTUXKART_DATADIR' not in environ:
+    environ['SUPERTUXKART_DATADIR'] = path.dirname(path.abspath(__file__))
 from .pystk_cpp import *
 from . import gui
